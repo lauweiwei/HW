741b0e4758bb9e44eeb8e957214928f78a97a87e
admin <admin@PM11-21>
2018-03-09 13:30:50 +0700

    hello world

---
+++ helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
